d4cbb77a1500f7c6ca5ded872db72240bfaa84df
loppanloppis <london@crashdiet.org>
2025-07-07 23:33:57 +0200

    Small tweaks to make this library work with modern compilers

---
+++ config.h
@@ -0,0 +1,24 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  */
+/* $Id$ */
+
+
+/* #undef SHORTRIGHT */
+#define COPYRIGHT "Copyright (C) 2003-2009 Michiel Broek, All Rights Reserved"
+#define VERSION "0.9.6"
+
+/* Name of package */
+/* #undef PACKAGE */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef mode_t */
+
+/* Define to `long' if <sys/types.h> does not define. */
+/* #undef off_t */
+
+/* Define to `unsigned' if <sys/types.h> does not define. */
+/* #undef size_t */
+
